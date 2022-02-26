a277484a8eec8d53d53df88575273ae333263212
Sancodes1 <100065380+Sancodes1@users.noreply.github.com>
2022-02-26 23:37:43 +0530

    Add files via upload

---
+++ DuplicateLead.java
@@ -0,0 +1,111 @@
+package Week2day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLead {
+
+	public static void main(String[] args) {
+		
+			//setup webdriver
+			WebDriverManager.chromedriver().setup();
+			ChromeDriver driver=new ChromeDriver();
+			
+			//open url
+			driver.get("http://leaftaps.com/opentaps/control/main");
+			//maximize window
+			driver.manage().window().maximize();
+			
+			//Enter UserName
+			WebElement elementUsername = driver.findElement(By.id("username"));
+			elementUsername.sendKeys("Demosalesmanager");
+			
+			//Enter  Password 
+			WebElement elementpassword = driver.findElement(By.id("password"));
+			elementpassword.sendKeys("crmsfa");
+			
+			//Click on Login Button
+			WebElement elementLoginButton = driver.findElement(By.className("decorativeSubmit"));
+			elementLoginButton.click();
+			
+			//Click on CRM/SFA Link
+			WebElement elementCRMSFA = driver.findElement(By.linkText("CRM/SFA"));
+			elementCRMSFA.click();
+			
+			// Click on Leads Button
+			WebElement elementLeads = driver.findElement(By.linkText("Leads"));
+			elementLeads.click();
+			
+			//Click on Create Lead 
+			WebElement elementCreateLead = driver.findElement(By.linkText("Create Lead"));
+			elementCreateLead.click();
+			
+			//Enter CompanyName Field
+			WebElement elementcreateLeadForm_companyName = driver.findElement(By.id("createLeadForm_companyName"));
+			elementcreateLeadForm_companyName.sendKeys("Google");
+			
+			//Enter FirstName Field
+			WebElement elementcreateLeadForm_firstName = driver.findElement(By.id("createLeadForm_firstName"));
+			elementcreateLeadForm_firstName.sendKeys("Santhosh");
+			
+			//Enter LastName
+			WebElement elementcreateLeadForm_lastName= driver.findElement(By.id("createLeadForm_lastName"));
+			elementcreateLeadForm_lastName.sendKeys("N");
+			
+			//Enter FirstName(Local)
+			WebElement elementcreateLeadForm_firstNameLocal= driver.findElement(By.id("createLeadForm_firstNameLocal"));
+			elementcreateLeadForm_firstNameLocal.sendKeys("Santhu");
+			
+			//Enter Department
+			WebElement elementcreateLeadForm_departmentName= driver.findElement(By.id("createLeadForm_departmentName"));
+			elementcreateLeadForm_departmentName.sendKeys("QualityAssurance");
+			
+			//Enter Description 
+			WebElement elementcreateLeadForm_description= driver.findElement(By.id("createLeadForm_description"));
+			elementcreateLeadForm_description.sendKeys("Testing script");
+			
+			//Enter your email
+			WebElement elementcreateLeadForm_primaryEmail= driver.findElement(By.id("createLeadForm_primaryEmail"));
+			elementcreateLeadForm_primaryEmail.sendKeys("santhoshn318@gmail.com");
+			
+			//Select State/Province
+			WebElement provinceElement = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+			Select province=new Select(provinceElement);
+			province.selectByVisibleText("New York");
+			
+			//Click on Create Button
+			WebElement elementsmallSubmit = driver.findElement(By.className("smallSubmit"));
+			elementsmallSubmit.click();
+			
+			//Get the Title of Resulting Page
+			System.out.println("The title is "+driver.getTitle());
+			
+			//Click on Duplicate lead
+			driver.findElement(By.linkText("Duplicate Lead")).click();
+			
+			
+			//Clear the CompanyName Field and Enter new CompanyName
+			driver.findElement(By.id("createLeadForm_companyName")).clear();
+			driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Amazon");
+			
+			
+			//Clear the FirstName Field using .clear() And Enter new FirstName
+			driver.findElement(By.id("createLeadForm_firstName")).clear();
+			driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Sancodes");
+			
+			//Click on Create Button
+	        driver.findElement(By.className("smallSubmit")).click();
+			elementsmallSubmit.click();
+        			
+			//Get the Title of Resulting Page
+			System.out.println("The title is "+driver.getTitle());
+			
+	}
+
+	}
+
+
